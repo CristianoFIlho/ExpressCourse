91b29e5f74c3a663a287837e206118c6fee2fc3f
Cristiano Filho <54041918+CristianoFIlho@users.noreply.github.com>
2021-09-21 13:00:38 -0300

    update

---
+++ 4-names.js
@@ -0,0 +1,11 @@
+//local
+
+const john = 'john'
+
+//shere
+const peter = 'peter'
+const secret = 'SUPER SECRET'
+
+module.exports = {}
+
+console.log(module)
--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-console.log(__dirname);
+/*console.log(__dirname);
 setInterval(() => {
     console.log('Hello world');
-}, 1000);
+}, 1000);*/
+
+const names = require('./4-names');
+console.log(names);
+
+
+sayHi('susan')
+sayHi(john)
+sayHi(peter)
