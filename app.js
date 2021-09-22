d273969c15b622102279c770ec48c3429b87b33c
Cristiano Filho <54041918+CristianoFIlho@users.noreply.github.com>
2021-09-22 13:55:16 -0300

    contaction

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const {
 } = require('fs');
 const fs = require('fs');
 
-const first = readFileSync('./content/first.txt', 'utf8');
+const first = readFileSync('/home/cristianofilho/Desktop/GIT/GitHub/ExpressCourse/content/fist.txt', 'utf8');
 
-const second = readFileSync('./content/subfolder/secundary.txt', 'utf8');
+const second = readFileSync('/home/cristianofilho/Desktop/GIT/GitHub/ExpressCourse/content/subfolder/secundary.txt', 'utf8');
 
 console.log(first, second);
