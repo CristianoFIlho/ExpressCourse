4f00fd7a888634b195992bd6db593019874e10f3
Cristiano Filho <54041918+CristianoFIlho@users.noreply.github.com>
2021-09-23 14:46:40 -0300

    syncronize

--- 10-fs-sync.js
+++ 10-fs-sync.js
@@ -3,11 +3,11 @@ const {
     writeFileSync
 } = require('fs')
 console.log('start')
-const first = readFileSync('./content/first.txt', 'utf8')
-const second = readFileSync('./content/second.txt', 'utf8')
+const first = readFileSync('/home/cristianofilho/Desktop/GIT/GitHub/ExpressCourse/content/fist.txt', 'utf8')
+const second = readFileSync('/home/cristianofilho/Desktop/GIT/GitHub/ExpressCourse/content/subfolder/secundary.txt', 'utf8')
 
 writeFileSync(
-    './content/result-sync.txt',
+    '/home/cristianofilho/Desktop/GIT/GitHub/ExpressCourse/content/result-sync.txt',
     `Here is the result : ${first}, ${second}`, {
         flag: 'a'
     }
